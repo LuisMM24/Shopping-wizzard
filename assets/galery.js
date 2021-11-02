343c645ac02a4466f773009916ddf47f27a121e9
Kory <50864697+Kory-ifcd0210@users.noreply.github.com>
2021-11-02 14:40:00 +0000

    Añadir funcionalidades Galery

--- assets/galery.js
+++ assets/galery.js
@@ -4,7 +4,8 @@ containerGalery.addEventListener('mouseover', showSlides);
 var imgColorLent=document.querySelectorAll('.colorLents');
 var Lent=document.querySelector('#Lents');
 Lent.addEventListener('click', changeColor);
-var prevTarget;
+var colorSelected= "";
+
 
 //Cambio imagen Principal Galería
 function showSlides(e){
@@ -14,6 +15,7 @@ function showSlides(e){
 //Cambio imagen principal color
 function changeColor(e){
     imgPrincipal.src=e.target.src;
+
 }
 
 // Añade Class selected a cada imagen
@@ -21,7 +23,25 @@ imgColorLent.forEach(element => { //Recorre la colección de imágenes
     element.addEventListener('click', (e)=>{ //crea un evento para cada imagen
         imgColorLent.forEach(element => {
             element.classList.remove ("selected")}); // elimina la clase de cada elemento de la imagen si ya está creada
-        e.target.classList.add("selected"); //Selecciona cada elemento con target, y le añade la clase selected a la imagen seleccionada;
+            e.target.classList.add("selected"); //Selecciona cada elemento con target, y le añade la clase selected a la imagen seleccionada;
+            colorSelected=e.target.name;
     }
     );
 });
+
+function decrement(){
+    var quantity=document.getElementById('quantity');
+    if (quantity.value<=1) return;
+    quantity.value = quantity.value -1;
+    quantity.innerHTML=quantity.value;
+}
+
+function increment(){
+    var quantity=document.getElementById('quantity');
+    quantity.value = parseInt(quantity.value) +1;
+    quantity.innerHTML=quantity.value;
+}
+
+function buyNow(){
+    
+}
