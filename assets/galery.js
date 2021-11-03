9550e8cf941012ce94ef73f20a7bf4b3cea527e3
Adrián Serrano <adrianno_3@hotmail.com>
2021-11-03 13:04:12 +0100

    validation v12

--- assets/galery.js
+++ assets/galery.js
@@ -7,18 +7,19 @@ Lent.addEventListener('click', changeColor);
 var colorSelected= "";
 var colorSelectedValidation=false;
 var ProductPage=document.querySelector("#product-page")
-
 //Cambio imagen Principal Galería
 function showSlides(e){
     imgPrincipal.src=e.target.src;
 }
-
 //Cambio imagen principal color
 function changeColor(e){
+    if(e.target.classList=="colorLents"){
     imgPrincipal.src=e.target.src;
-
+    }
+    else{
+        return;
+    }
 }
-
 // Añade Class selected a cada imagen
 imgColorLent.forEach(element => { //Recorre la colección de imágenes
     element.addEventListener('click', (e)=>{ //crea un evento para cada imagen
@@ -27,12 +28,9 @@ imgColorLent.forEach(element => { //Recorre la colección de imágenes
             e.target.classList.add("selected"); //Selecciona cada elemento con target, y le añade la clase selected a la imagen seleccionada;
             colorSelected=e.target.name;
             colorSelectedValidation=true;
-
     }
     );
 });
-
-
 // Cambio de cantidad de compra
 function decrement(){
     var quantity=document.getElementById('quantity');
@@ -40,19 +38,16 @@ function decrement(){
     quantity.value = quantity.value -1;
     quantity.innerHTML=quantity.value;
 }
-
 function increment(){
     var quantity=document.getElementById('quantity');
     quantity.value = parseInt(quantity.value) +1;
     quantity.innerHTML=quantity.value;
 }
-
 //size
 let sizeGlass1="";
 let sizeGlassValidation=false;
 let sizeGlass=document.querySelector("#sizes-glass")
 sizeGlass.addEventListener("click",sizesValue)
-
 function sizesValue(){
     if (sizeGlass.value == ""){
         // console.log(sizeGlass1 + "REQUERIDO");
@@ -68,27 +63,34 @@ function sizesValue(){
     }
 }
 //Validation product page
-
 let stepLine=document.querySelector("#step-line");
 let buyNowButton=document.querySelector("#buyNow");
-buyNowButton.addEventListener("click",buyNow);
-
-
+let buyNowParent=document.querySelector(".buyNow-parent")
+//buyNowButton.addEventListener("click",buyNow);
+sizeGlass.addEventListener("click",ValidationValueCheck);
+Lent.addEventListener("click",ValidationValueCheck);// click function
+function ValidationValueCheck(){//validation function
+    if(buyNowParent.children.length>1){ //if parent has more than 1 child
+    buyNowParent.removeChild(buyNowParent.lastChild);//delete the messege box
+    }
+}
 function buyNow(){
     if(sizeGlassValidation==false||colorSelectedValidation==false){
+        ValidationValueCheck();
         let Val;
         Val = document.createElement('div');
         Val.classList.add('validation');
         Val.innerHTML = `Please select size, color and quantity`;
-        main.insertAdjacentElement('afterbegin', Val);
+        buyNowParent.appendChild(Val);
         return;
     }else{
+        ValidationValueCheck()
         ProductPage.classList.add("Hidden");
         stepLine.classList.remove("Hidden");
         profileForm.classList.remove("Hidden");
-        console.log("quantity.value "+quantity.value)
-        console.log("colorSelected "+colorSelected)
-        console.log("imgPrincipal.src "+imgPrincipal.src)
-        console.log("sizeGlass1 "+sizeGlass1)
+        console.log("quantity.value "+quantity.value);
+        console.log("colorSelected "+colorSelected);
+        console.log("imgPrincipal.src "+imgPrincipal.src);
+        console.log("sizeGlass1 "+sizeGlass1);
     }
 }
